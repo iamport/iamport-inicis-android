9f81e966c86747c26dfe8b75f3716ce0186b00d7
jang <jang@siot.do>
2016-05-09 20:52:23 +0900

    ISP(ISP카드결제 인증앱), BANKPAY(실시간계좌이체 인증앱) 미설치된 단말에서 ActivityNotFoundException이 발생했을 때 처리(packageName이 추출되지 않는 경우가 있음)

--- src/com/siot/iamportsdk/InicisWebViewClient.java
+++ src/com/siot/iamportsdk/InicisWebViewClient.java
@@ -34,6 +34,8 @@ public class InicisWebViewClient extends WebViewClient {
 			} catch (ActivityNotFoundException e) {
 				if ( intent == null )	return false;
 				
+				if ( handleNotFoundPaymentScheme(intent.getScheme()) )	return true;
+				
 				String packageName = intent.getPackage();
 		        if (packageName != null) {
 		            activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName)));
@@ -42,6 +44,27 @@ public class InicisWebViewClient extends WebViewClient {
 		        
 		        return false;
 			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * @param scheme
+	 * @return 해당 scheme에 대해 처리를 직접 하는지 여부
+	 * 
+	 * 결제를 위한 3rd-party 앱이 아직 설치되어있지 않아 ActivityNotFoundException이 발생하는 경우 처리합니다.
+	 * 여기서 handler되지않은 scheme에 대해서는 intent로부터 Package정보 추출이 가능하다면 다음에서 packageName으로 market이동합니다.  
+	 * 
+	 */
+	protected boolean handleNotFoundPaymentScheme(String scheme) {
+		//PG사에서 호출하는 url에 package정보가 없어 ActivityNotFoundException이 난 후 market 실행이 안되는 경우
+		if ( PaymentScheme.ISP.equalsIgnoreCase(scheme) ) {
+			activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + PaymentScheme.PACKAGE_ISP)));
+			return true;
+		} else if ( PaymentScheme.BANKPAY.equalsIgnoreCase(scheme) ) {
+			activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + PaymentScheme.PACKAGE_BANKPAY)));
+			return true;
 		}
 		
 		return false;
---
+++ src/com/siot/iamportsdk/PaymentScheme.java
@@ -0,0 +1,12 @@
+package com.siot.iamportsdk;
+
+public class PaymentScheme {
+
+	public final static String ISP = "ispmobile";
+	public final static String BANKPAY = "kftc-bankpay";
+	public final static String HYUNDAI_APPCARD = "hdcardappcardansimclick"; //intent:hdcardappcardansimclick://appcard?acctid=201605092050048514902797477441#Intent;package=com.hyundaicard.appcard;end;
+	
+	public final static String PACKAGE_ISP = "kvp.jjy.MispAndroid320";
+	public final static String PACKAGE_BANKPAY = "com.kftc.bankpay.android";
+	
+}
